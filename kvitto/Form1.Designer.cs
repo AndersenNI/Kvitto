f9a248011de83299c8dcc4847c996cf8b6f70fac
Andersen <Andersen.ni@outlook.com>
2015-03-09 16:09:31 +0100

    p

--- kvitto/Form1.Designer.cs
+++ kvitto/Form1.Designer.cs
@@ -49,12 +49,16 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.buttoncleartext = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtboxNamn
@@ -232,10 +236,11 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1143, 603);
+            this.tabControl1.Size = new System.Drawing.Size(441, 423);
             this.tabControl1.TabIndex = 26;
             // 
             // tabPage1
@@ -258,7 +263,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1135, 577);
+            this.tabPage1.Size = new System.Drawing.Size(433, 397);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Print";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -274,20 +279,20 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1135, 577);
+            this.tabPage2.Size = new System.Drawing.Size(433, 397);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Settings";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // buttoncleartext
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(345, 324);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(67, 17);
-            this.checkBox1.TabIndex = 26;
-            this.checkBox1.Text = "ON/OFF";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.buttoncleartext.Location = new System.Drawing.Point(345, 62);
+            this.buttoncleartext.Name = "buttoncleartext";
+            this.buttoncleartext.Size = new System.Drawing.Size(75, 23);
+            this.buttoncleartext.TabIndex = 28;
+            this.buttoncleartext.Text = "Clear text";
+            this.buttoncleartext.UseVisualStyleBackColor = true;
+            this.buttoncleartext.Click += new System.EventHandler(this.buttoncleartext_Click);
             // 
             // label3
             // 
@@ -300,22 +305,52 @@
             this.label3.TabIndex = 27;
             this.label3.Text = "The Symbols must remain for the program to work";
             // 
-            // buttoncleartext
+            // checkBox1
             // 
-            this.buttoncleartext.Location = new System.Drawing.Point(345, 62);
-            this.buttoncleartext.Name = "buttoncleartext";
-            this.buttoncleartext.Size = new System.Drawing.Size(75, 23);
-            this.buttoncleartext.TabIndex = 28;
-            this.buttoncleartext.Text = "Clear text";
-            this.buttoncleartext.UseVisualStyleBackColor = true;
-            this.buttoncleartext.Click += new System.EventHandler(this.buttoncleartext_Click);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(345, 324);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 26;
+            this.checkBox1.Text = "ON/OFF";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(433, 397);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Local Hantering";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(7, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Kvitto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(445, 415);
             this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -326,6 +361,8 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -356,6 +393,9 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttoncleartext;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
